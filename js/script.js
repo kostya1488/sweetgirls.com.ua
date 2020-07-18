576f75a221c33fc1d63ca53da0e57457c27000ac
kostya <matvienkoigor956@gmail.com>
2020-07-18 17:57:31 +0300

    set the styles for the mob menu

--- js/script.js
+++ js/script.js
@@ -9,3 +9,19 @@ var Swipes = new Swiper('.swiper-container', {
         el: '.swiper-pagination',
     },
 });
+// -------------------------------
+$('.toggle').click(function(e) {
+    e.preventDefault();
+
+    var $this = $(this);
+
+    if ($this.next().hasClass('show')) {
+        $this.next().removeClass('show');
+        $this.next().slideUp(350);
+    } else {
+        $this.parent().parent().find('.inner').removeClass('show');
+        $this.parent().parent().find('.inner').slideUp(350);
+        $this.next().toggleClass('show');
+        $this.next().slideToggle(350);
+    }
+});
