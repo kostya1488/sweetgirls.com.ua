720de2c407d71ca2ac0a2af6b8bc3a208fe9ba75
kostya <matvienkoigor956@gmail.com>
2020-07-20 11:25:16 +0300

    added animation for switching accordion elements

--- js/script.js
+++ js/script.js
@@ -14,11 +14,18 @@ $('.toggle').click(function(e) {
     e.preventDefault();
 
     var $this = $(this);
+    $this.addClass('toggle_active');
 
-    if ($this.next().hasClass('show')) {
+    if ($this.next().hasClass('show') && $this.hasClass('toggle_active')) {
+        $this.removeClass('toggle_active');
+
         $this.next().removeClass('show');
         $this.next().slideUp(350);
     } else {
+        $this.parent().parent().find('.toggle').removeClass('toggle_active');
+        $this.addClass('toggle_active');
+
+
         $this.parent().parent().find('.inner').removeClass('show');
         $this.parent().parent().find('.inner').slideUp(350);
         $this.next().toggleClass('show');
